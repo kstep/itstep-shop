8ec5ae95db7189195710cdbebafcfaa30fa5d826
PseudoHyena <anzen.bagnyuk@gmail.com>
2019-05-15 21:30:54 +0300

    add sort & filter for catalog

---
+++ shop/catalog/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+
+
+class ItemSortForm(forms.Form):
+    order_by = forms.ChoiceField(choices=[
+        ('name', 'Name'),
+        ('-name', 'Name (Z-A)'),
+        ('price', 'Price'),
+        ('-price', 'Price (high to low)'),
+        ('count', 'count'),
+    ], label='Order')
+
+
+class ItemFilterForm(forms.Form):
+    name__contains = forms.CharField(
+        max_length=200,
+        required=False,
+        label='Name')
+    price__gt = forms.DecimalField(
+        max_digits=30,
+        decimal_places=2,
+        required=False,
+        label='Price greater than'
+    )
+    price__lt = forms.DecimalField(
+        max_digits=30,
+        decimal_places=2,
+        required=False,
+        label='Price less than'
+    )
--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -1,11 +1,31 @@
+from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from catalog.forms import ItemSortForm, ItemFilterForm
 from catalog.models import Item
 
 
-def item_list(request):
-    return render(request, 'item_list.html', {'items': Item.objects.all()})
+def item_list(request: HttpRequest):
+    item_sort_form = ItemSortForm(request.GET)
+    item_filter_form = ItemFilterForm(request.GET)
+
+    filters = {
+        k: v for (k, v) in
+        item_filter_form.cleaned_data.items()
+        if v is not None
+    } if item_filter_form.is_valid() else {}
+
+    return render(request, 'item_list.html',
+                  {
+                      'item_sort_form': item_sort_form,
+                      'item_filter_form': item_filter_form,
+                      'items': Item.objects
+                                   .filter(**filters)
+                                   .order_by(
+                                        item_sort_form.cleaned_data['order_by']
+                                         if item_sort_form.is_valid() else 'name')
+                  })
 
 def item_detail(request, pk):
     return render(request, 'item_detail.html',
