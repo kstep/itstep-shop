06fb937a40ae47f2dc3b61f4ffc289434b39bc80
John Doe <asdfas@asdfas.ru>
2019-06-13 21:40:45 +0300

    order model

--- shop/orders/models.py
+++ shop/orders/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
 from catalog.models import ItemAmount
 
 
@@ -10,5 +12,19 @@ class OrderItem(ItemAmount):
                               related_name='items')
 
 
+class PaymentType(models.Model):
+    name = models.CharField(max_length=200)
+
 class Order(models.Model):
-    shipping = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    tel = models.CharField(max_length=50)
+    email = models.EmailField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    address = models.TextField()
+    payment_type = models.ForeignKey(PaymentType,
+                                     on_delete=models.CASCADE)
+    state = models.CharField(max_length=20)
+
+
+
