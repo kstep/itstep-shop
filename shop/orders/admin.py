ef883958edcb75e8eb9ba74f20edc9ee03e37187
John Doe <asdfas@asdfas.ru>
2019-06-18 21:38:45 +0300

    order state (wip)

--- shop/orders/admin.py
+++ shop/orders/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
 # Register your models here.
+from orders import models
+from orders.models import InvalidOrderStateError
+
+
+class OrderItemInline(admin.TabularInline):
+    model = models.OrderItem
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+    exclude = ['state']
+    actions = ['review',
+               'accept',
+               'reject',
+               'ship',
+               'deliver',
+               'drop']
+
+    def transfer_state(self, newstate, queryset):
+        for order in queryset:
+            try:
+                method = getattr(order, newstate)
+                method()
+            except InvalidOrderStateError:
+                pass
+            order.save()
+
+    def review(self, request, queryset):
+        self.transfer_state('review', queryset)
+    def accept(self, request, queryset):
+        self.transfer_state('accept', queryset)
+    def reject(self, request, queryset):
+        self.transfer_state('reject', queryset)
+
+
+
+@admin.register(models.PaymentType)
+class PaymentTypeAdmin(admin.ModelAdmin):
+    pass
--- shop/orders/models.py
+++ shop/orders/models.py
@@ -6,6 +6,16 @@ from django.utils import timezone
 from catalog.models import ItemAmount
 
 
+class InvalidOrderStateError(RuntimeError):
+    def __init__(self, old_state, new_state):
+        self.old_state = old_state
+        self.new_state = new_state
+
+    def __str__(self):
+        return 'Invalid order state transition: ' + \
+            f'{self.old_state} -> {self.new_state}'
+
+
 class OrderItem(ItemAmount):
     order = models.ForeignKey('Order',
                               on_delete=models.CASCADE,
@@ -14,6 +24,9 @@ class OrderItem(ItemAmount):
 
 class PaymentType(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
@@ -24,7 +37,53 @@ class Order(models.Model):
     address = models.TextField()
     payment_type = models.ForeignKey(PaymentType,
                                      on_delete=models.CASCADE)
-    state = models.CharField(max_length=20)
 
+    STATES = [
+        ('new', 'New order'),
+        ('in_review', 'In review'),
+        ('accepted', 'Order accepted'),
+        ('rejected', 'Order rejected'),
+        ('shipping', 'Shipping order'),
+        ('delivered', 'Order delivered'),
+        ('dropped', 'Order delivery failed')
+    ]
 
+    state = models.CharField(max_length=20,
+                             choices=STATES,
+                             default='new')
 
+    def review(self):
+        if self.state == 'new':
+            self.state = 'in_review'
+        else:
+            raise InvalidOrderStateError(self.state, 'in_review')
+
+    def accept(self):
+        if self.state == 'in_review':
+            self.state = 'accepted'
+        else:
+            raise InvalidOrderStateError(self.state, 'accepted')
+
+    def reject(self):
+        if self.state == 'in_review':
+            self.state = 'rejected'
+        else:
+            raise InvalidOrderStateError(self.state, 'rejected')
+
+    def ship(self):
+        if self.state == 'accepted':
+            self.state = 'shipping'
+        else:
+            raise InvalidOrderStateError(self.state, 'shipping')
+
+    def deliver(self):
+        if self.state == 'shipping':
+            self.state = 'delivered'
+        else:
+            raise InvalidOrderStateError(self.state, 'delivered')
+
+    def drop(self):
+        if self.state == 'shipping':
+            self.state = 'dropped'
+        else:
+            raise InvalidOrderStateError(self.state, 'dropped')
